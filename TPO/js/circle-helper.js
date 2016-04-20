5dba61b17d93fc06ac6bfaa0d399649c394dc734
ysm1180 <ysm1180@snu.ac.kr>
2016-04-21 04:50:35 +0900

    First basic setting
    First basic code and style for main page

---
+++ TPO/js/circle-helper.js
@@ -0,0 +1,32 @@
+/*global tau */
+/*jslint unparam: true */
+(function(tau) {
+	'use strict';
+	var page, elScroller, list, listHelper = [], i, len;
+
+	if (tau.support.shape.circle) {
+		document.addEventListener("pagebeforeshow", function(e) {
+			page = e.target;
+			elScroller = page.querySelector(".ui-scroller");
+			if (elScroller) {
+				list = elScroller.querySelectorAll(".ui-listview");
+				if (list) {
+					len = list.length;
+					for (i = 0; i < len; ++i) {
+						listHelper[i] = tau.helper.SnapListStyle.create(list[i], {animate: "scale"});
+					}
+				}
+			}
+		});
+
+		document.addEventListener("pagebeforehide", function(e) {
+			len = listHelper.length;
+			if (len) {
+				for (i = 0; i < len; i++) {
+					listHelper[i].destroy();
+				}
+				listHelper = [];
+			}
+		});
+	}
+}(tau));
