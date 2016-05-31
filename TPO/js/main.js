2271f71d574699a6680bfe7361b20b7b004a5ea4
ysm1180 <ysm1180@snu.ac.kr>
2016-05-31 17:43:38 +0900

    implement delete favorite, fix widget (not finished)

--- TPO/js/main.js
+++ TPO/js/main.js
@@ -15,12 +15,14 @@ function keyEventHandler(event) {
 		} else if (pageid === "surroundingBusStation" || pageid === "busFavorite") {
 			window.history.go(-2);
 		} else if (pageid === "busArrivalTime" || pageid === "busNumberStationList") {
-			window.history.go(-2);
+			window.history.go(-3);
 		} else if (pageid === "surroundingSubwayStation" || pageid === "lvAllSubwayStation") { // subway
 			window.history.go(-2);
 		} else if (pageid === "processing") {
 			window.history.go(-1);
 		} else if (pageid === "busArrivalTime_MoreOptions") {
+			tau.closePopup(popup);
+		} else if (pageid === "showFavoriteStation_MoreOptions") {
 			tau.closePopup(popup);
 		} else {
 			window.history.back();
@@ -31,8 +33,11 @@ function keyEventHandler(event) {
 function init() {
 	var marqueeSurrounding = new MARQUEELIST(),
 		marqueeStation = new MARQUEELIST(),
+		marqueeFavorite = new MARQUEELIST(),
 		marqueeWidget,
-		moreoption = new MOREOPTION();
+		moreoption = new MOREOPTION(),
+		moreoptionFavorite = new MOREOPTION();
+	
 	
 	// 버스 정류장 ID 입력
 	document.getElementById('inputBusID').addEventListener('pagebeforeshow', function() {
@@ -107,7 +112,7 @@ function init() {
 			} else {
 				dataTitle = target.getAttribute("data-title");
 				if (dataTitle === "즐겨찾기 등록") {
-					tau.closePopup(this.popup);
+					tau.closePopup(moreoption.popup);
 					bus.showFavoriteBus();
 				}
 			}
@@ -115,6 +120,32 @@ function init() {
 	});
 	document.getElementById('busArrivalTime').addEventListener('pagebeforehide', function() {
 		moreoption.pageBeforeHideHandler();
+	});
+	document.getElementById('showFavoriteStation').addEventListener('pagebeforeshow', function() {
+		moreoptionFavorite.pageBeforeShowHandler('showFavoriteStation', function(event) {
+			var target = event.target,
+			dataTitle,
+			dataIndex;
+
+			if (target.classList.contains("ui-selector-indicator")) {
+				dataIndex = target.getAttribute("data-index");
+				if (dataIndex === "0") {
+					tau.closePopup(moreoptionFavorite.popup);
+					bus.changeToDeleteModeOnFavorite("showFavoriteStation");
+				} else {
+					tau.closePopup(moreoptionFavorite.popup);
+				}
+			} else {
+				dataTitle = target.getAttribute("data-title");
+				if (dataTitle === "삭제") {
+					tau.closePopup(moreoptionFavorite.popup);
+					bus.changeToDeleteModeOnFavorite("showFavoriteStation");
+				}
+			}
+		});
+	});
+	document.getElementById('showFavoriteStation').addEventListener('pagebeforehide', function() {
+		moreoptionFavorite.pageBeforeHideHandler();
 	});
 	
 	
@@ -131,6 +162,12 @@ function init() {
 	document.getElementById('busNumberStationList').addEventListener('pagebeforehide', function() {
 		marqueeStation.pageBeforeHideHandler();
 	});
+	document.getElementById('showFavoriteStation').addEventListener('pagebeforeshow', function() {
+		marqueeFavorite.pageBeforeShowHandler('showFavoriteStation');
+	});
+	document.getElementById('showFavoriteStation').addEventListener('pagebeforehide', function() {
+		marqueeFavorite.pageBeforeHideHandler();
+	});
 	
 	// 주변 정류장 클릭
 	document.getElementById('searchSurrounding').addEventListener('click', function() {
@@ -140,6 +177,25 @@ function init() {
 	// 즐겨찾기 등록
 	document.getElementById('btnRegister').addEventListener('click', function() {
 		bus.registerFavoriteBus();
+	});
+	
+	// 즐겨찾기 삭제
+	document.getElementById('btnDelete').addEventListener('click', function() {
+		var page = document.getElementById('showFavoriteStation'),
+		listview = document.querySelector('#' + pageId + ' .ui-listview'),
+		list = listview.getElementsByTagName("li"),
+		listLength = list.length
+		i,
+		j = 0,
+		id = [];
+		
+		for (i = 0; i < listlength; ++i) {
+			if (list[i].classList.contains("select")) {
+				id[j++] = list[i].id;
+			}
+		}
+		
+		bus.deleteFavorite(id);
 	});
 
 	// 등록한 즐겨찾기 보기
