a2dccfec339655f16408ae48299ba461a601e10b
ysm1180 <ysm1180@snu.ac.kr>
2016-06-16 00:36:51 +0900

    add GPS file
    compact loading GPS part in bus and subway to GPS functions in a file

--- TPO/js/subway.js
+++ TPO/js/subway.js
@@ -442,33 +442,10 @@ function successCallback(position) {
 		});
 }
 
-function errorCallback(error) {
-	switch (error.code) {
-	case error.PERMISSION_DENIED:
-		toastPopup.openCheckPopup("GPS 권한을 허용해주세요.", true);
-		break;
-	case error.POSITION_UNAVAILABLE:
-		toastPopup.openCheckPopup("연결된 디바이스의 GPS를 켜주세요.", true);
-		break;
-	case error.TIMEOUT:
-		toastPopup.openPopup("GPS 요청 시간이 초과되었습니다.", true);
-		break;
-	case error.UNKNOWN_ERROR:
-		toastPopup.openPopup("알 수없는 오류가 발생했습니다.", true);
-		break;
-	}
-}
 
 /**
  * GPS를 이용하여 1km 반경 내의 주변 지하철 역을 조회한다.
  */
 SUBWAY.prototype.showSurroundingStationsByGps = function() {
-	if (navigator.geolocation) {
-		tau.changePage("#processing");
-		navigator.geolocation.getCurrentPosition(successCallback, errorCallback, {
-			timeout : 20000
-		});
-	} else {
-		toastPopup.openPopup("GPS를 지원하지 않는 기기입니다.");
-	}
+	loadGPS(successCallback);
 };
